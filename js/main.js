b300bda3d2ce4cc7862546533c73f8e0fa509915
Tony <tonylu23@gmail.com>
2020-01-10 10:22:38 -0500

    add documentation and remove redundant code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const attribute = 'flag-icon flag-icon-' // the class for the flag
 
-// Track left side
+// Track when left's currency changes
 document.querySelector('#currency-left').addEventListener('change', (e) => {
-    const currencyCode = e.target.value // the country's currency code
     const countryCode = e.target.selectedOptions[0].id // the country's code
 
     // Change text
@@ -10,90 +9,94 @@ document.querySelector('#currency-left').addEventListener('change', (e) => {
     
     // Change the flag
     changeFlag('left', countryCode)
+
+    // Set amount to empty
+    amountInput('left').value = ''
+    amountInput('right').value = ''
 })
 
-// Track right side
+// Track right side's currency changes
 document.querySelector('#currency-right').addEventListener('change', (e) => {
-    const currencyCode = e.target.value // the country's currency code
     const countryCode = e.target.selectedOptions[0].id // the country's code
-
+    
     // Change text
     changeText('right', countryCode)
-
+    
     // Change the flag
     changeFlag('right', countryCode)
+
+    // Set amount to empty
+    amountInput('left').value = ''
+    amountInput('right').value = ''
 })
 
+// dynamic rate updates
 document.querySelector('#left-currency-amount').addEventListener('input', () => {
-    const leftCurrencyCode = document.querySelector('#currency-left')
-    const rightCurrencyCode = document.querySelector('#currency-right')
-    const amountInput = document.querySelector('#left-currency-amount')
-    const amountOutput = document.querySelector('#right-currency-amount')
-
-    calculateRate(leftCurrencyCode, rightCurrencyCode, amountInput, amountOutput)
+    calculateRate(leftCurrencyCode(), rightCurrencyCode(), amountInput('left'), amountOutput('right'))
 })
 
+// dynamic rate updates
 document.querySelector('#right-currency-amount').addEventListener('input', () => {
-    const leftCurrencyCode = document.querySelector('#currency-left')
-    const rightCurrencyCode = document.querySelector('#currency-right')
-    const amountInput = document.querySelector('#right-currency-amount')
-    const amountOutput = document.querySelector('#left-currency-amount')
-
-    calculateRate(rightCurrencyCode, leftCurrencyCode, amountInput, amountOutput)
+    calculateRate(leftCurrencyCode(), rightCurrencyCode(), amountInput('right'), amountOutput('left'))
 })
 
+// swap the currencies
 document.querySelector('.exchange').addEventListener('mousedown', () => {
     swapSides()
 })
 
-// fromCurr = document.querySelector('#currency-left')
-// toCurr = document.querySelector('#currency-right')
-// fromInput = document.querySelector('#left-currency-amount')
-// toInput = document.querySelector('#right-currency-amount')
+const leftCurrencyCode = () => document.querySelector('#currency-left')
+const rightCurrencyCode = () => document.querySelector('#currency-right')
+const amountInput = (side) => document.querySelector(`#${side}-currency-amount`)
+const amountOutput = (side) => document.querySelector(`#${side}-currency-amount`)
 
+// fromCurrencyCode = the currency code (USD)
+// toCurrencyCode = the currency to convert to (CAD)
+const getRate = async (fromCurrencyCode, toCurrencyCode) => {
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrencyCode}`)
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.rates[`${toCurrencyCode}`]
+    } else {
+        throw new Error('Currency not supported or something else happened.')
+    }
+}
+
+// fromCurrency = DOM object that holds currency code to convert from
+// toCurrency = DOM object that holds currency code to convert to
+// fromInput = DOM text field that holds the number value to convert from
+// toOutput = DOM text field that holds the number value to convert to
 const calculateRate = (fromCurrency, toCurrency, fromInput, toInput) => {
-    // get the currency code
-    const fromCurrencyCode = fromCurrency.value
-    const toCurrencyCode = toCurrency.value
-
-    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrencyCode}`).then((response) => response.json()).then((response) => {
-        // grab the exchange rate
-        const exchangeRate = response.rates[toCurrencyCode]
-
-        // set the new amount to the calculate rate
-        toInput.value = (fromInput.value * exchangeRate).toFixed(2)
+    getRate(`${fromCurrency.value}`, `${toCurrency.value}`).then((rate) => {
+        const convertedRate = (fromInput.value * rate).toFixed(2)
+        toInput.value = convertedRate
+    }).catch((err) => {
+        console.log(err)
     })
 }
 
 const swapSides = () => {
-    // gather left side
-    let left = document.querySelector('#currency-left')
-    const leftCountryCode = left.selectedOptions[0].id
-    const leftCurrencyCode = left.value
+    // gather left side money value
     let leftMoney = document.querySelector('#left-currency-amount')
     
-    // gather right side
-    let right = document.querySelector('#currency-right')
-    const rightCountryCode = right.selectedOptions[0].id
-    const rightCurrencyCode = right.value
+    // gather right side money value
     let rightMoney = document.querySelector('#right-currency-amount')
     
-    // temp setup
-    const tempCountryCode = leftCountryCode
-    const tempCurrencyCode = leftCurrencyCode
+    // temp vars setup
+    const tempCountryCode = leftCurrencyCode().selectedOptions[0].id
+    const tempCurrencyCode = leftCurrencyCode().value
     const tempMoney = leftMoney.value
     
-    
     // set left side with the right's
-    changeText('left', rightCountryCode) // change the text
-    changeFlag('left', rightCountryCode) // change the flag
-    left.value = rightCurrencyCode // update the selector
+    changeText('left', rightCurrencyCode().selectedOptions[0].id) // change the text
+    changeFlag('left', rightCurrencyCode().selectedOptions[0].id) // change the flag
+    leftCurrencyCode().value = rightCurrencyCode().value // update the selector
     leftMoney.value = rightMoney.value // change the input value
     
     // set right side
     changeText('right', tempCountryCode)
     changeFlag('right', tempCountryCode)
-    right.value = tempCurrencyCode
+    rightCurrencyCode().value = tempCurrencyCode
     rightMoney.value = tempMoney
 }
 
